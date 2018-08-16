7530fcf3d94c7164506fbdbcff18ef2ffdc89612
LC1301325 <LC1301325>
2018-08-16 09:51:47 -0400

    first commit

---
+++ EjemploHolaMundo/src/NewMain.java
@@ -0,0 +1,10 @@
+
+public class NewMain {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Ejemplo para GitHub/GitLab");
+        }
+    
+}
